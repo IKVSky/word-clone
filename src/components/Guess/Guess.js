b33569222dc247a35c05e0f2b7ecc7bf2fb39858
IKVSky <iliya.kalinov@sky.uk>
2023-04-06 11:56:39 +0100

    Exercise 4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,29 @@
 import React from "react";
 import { range  } from "../../utils";
+import { checkGuess } from '../../../src/game-helpers';
 
-function Guess({ guess }) {
+
+const Cell = ({ letter, status }) => {
+  const cellClasses = status ? `cell ${status}` : 'cell';
+
+  return (
+    <span className={cellClasses}>
+      {letter}
+    </span>
+  );
+};
+
+function Guess({ guess, answer }) {
+  const checkedGuess = checkGuess(guess, answer);
+
   return (
     <p className="guess">
       {range(5).map(num => (
-        <span key={num} className="cell">
-          {guess ? guess[num] : undefined}
-        </span>
+        <Cell 
+          key={num}
+          letter={checkedGuess ? checkedGuess[num].letter : undefined} 
+          status={checkedGuess ? checkedGuess[num].status : undefined} 
+        />
       ))}
     </p>
   );
